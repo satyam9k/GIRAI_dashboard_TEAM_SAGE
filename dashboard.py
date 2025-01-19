cc1f728eeeeae421c29d46f889181cf3f40c2645
Priyansha Upadhyay <77534121+Priyansha13@users.noreply.github.com>
2025-01-20 02:00:36 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -308,15 +308,6 @@ with col4:
             bgcolor='black'
         ),
         showlegend=True,
-        legend=dict(
-            x=1,  # Position on the right edge
-            y=0,  # Position at the bottom
-            xanchor='right',
-            yanchor='bottom',
-            font=dict(size=10, color='white'),
-            bgcolor='rgba(0,0,0,0)',  # Transparent background for the legend
-        ),        
-        # showlegend=True,
         margin=dict(l=0, r=0, t=30, b=0)
     )
     
