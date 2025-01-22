39d193f6d59fb6016c48d89a7714e500fd162f34
satyam9k <75788300+satyam9k@users.noreply.github.com>
2025-01-22 23:41:02 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -77,7 +77,7 @@ st.markdown(
 st.markdown(
     """
     <div class="centered-title">
-        🤖🗺️Uneven Progress: Responsible AI in the Global Landscape (Theme 3: Open Theme)🗺️🤖
+        🤖Uneven Progress: Responsible AI in the Global Landscape (Theme 3: Open Theme)🌎
     </div>
     """,
     unsafe_allow_html=True
