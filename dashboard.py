5971a7af7ed0de5e580c34ad1bcbb607f3e17ebb
satyam9k <75788300+satyam9k@users.noreply.github.com>
2025-03-03 09:54:06 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -472,7 +472,7 @@ with col3:
         y=thematic_by_development.index,
         text=np.round(thematic_by_development.values, 2),
         texttemplate='%{text}',  # This keeps the text labels in the heatmap
-        textfont={"color": "black","size": 10}, ##changed here
+        textfont={"color": "black","size": 14}, ##changed here
         colorscale='RdBu',
         showscale=True,
         hoverongaps=False,
